0051984c8fe414c400771f839e246afec6526f02
Ollohberdi <85603736+shokirovollohberdi@users.noreply.github.com>
2022-07-06 07:31:25 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,34 @@
+void main() {
+  /* 
+String ozgaruvchi
+  String ism = "Ollohberdi";
+  int yosh = 17;
+  print("Hi I'm " + ism + " and " + yosh.toString() + " yearsold");
+
+  int c = 9;
+  // int.parse("string") ushbu method ga string qiymat berilganda osha stringni int ga aylantirib beradi
+  print(int.parse("10") + c);
+  print(double.parse("10.1") + c);
+  //length - uzunlik
+  print(ism+" ning lenght " +ism.length.toString());
+*/
+
+  /*
+   Boolean ozgaruvchi
+  int a = 5;
+  int b = 5;
+  bool c = a>b; // A B dan kotta 
+  print(a==b); // == tengmi 
+  print(a!=b); // ! bu belgi inkor yani A  B ga teng emasmi degani
+  print(c);
+  print(a>=b); //  A son B sondan kotta yoki teng
+
+  bool f = true;
+  bool j = !f;
+  bool g = !(!f);
+  print(g);
+  print(j);
+
+ */
+  
+}
